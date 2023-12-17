17c527fb207ff7dd6410cb706a037fbf02e887d6
Sandy <sand.hart5@gmail.com>
2023-12-17 09:23:05 -0600

    1st JS program with JS & console.log()

--- hello.js
+++ hello.js
@@ -4,19 +4,36 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello World!" + " " + "Huzzah!!");
+console.log("Merry Christmas!");
+console.log('Quote the Raven "Nevermore" ');
+console.log('I am learning... Slowly, but surely!')
+console.log(167 * 4)
+
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+         Merry Christmas!
    2. Figure out what the parentheses do. Will the code work without them?
+         I believe it treated the words like code/commands/functions and not like a string to be printed.
+         ***Function parameters are listed inside the parentheses () in the function definition. Function arguments are the values received by the function when it is invoked.
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
+         The words won't print without the quotations.
+         There is no difference between using "" or '' quotation marks.
+         ***Quotation Marks create strings or literal characters.
+   4. Remove the semi-colon, ;.  
+         --Error
    5. Print a number. (Bonus: Print two numbers added together).
+         2 ... printed 2, then 2 + 2... printed 4 (did not use '' marks to treat the numbers like numbers and not strings)
    6. Print multiple messages one after the other.
+         Hello World!
+         Merry Christmas!
+         Huzzah!
    7. Print two messages on the same line.
+         Did it, then added a space to separate the messages
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
